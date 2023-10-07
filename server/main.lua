d187fd53b69a17a0af0edd3662bff6b0d40dfb36
Jake Landrum <jakelandrum2004@gmail.com>
2023-10-07 10:55:02 -0500

    nameChecking

--- server/main.lua
+++ server/main.lua
@@ -1,9 +1,5 @@
-local function StartFeatherWeapons()
-    StartAPI()
-end
-
 if GetCurrentResourceName() ~= "feather-weapons" then
     error("ERROR feather-weapons failed to load if you are a player report this to the server owner, if you are the server owner Feather Weapons resource must be named feather-weapons")
 else
-    StartFeatherWeapons()
+    --start core
 end
