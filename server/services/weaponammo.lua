3d85bb9cdfd3a761ca0f1fa537ce6bebc578f750
jakeyboi1 <jakelandrum2004@gmail.com>
2024-07-27 16:13:08 -0500

    maxammo

--- server/services/weaponammo.lua
+++ server/services/weaponammo.lua
@@ -5,22 +5,23 @@ function AmmoAPI.AddAmmo(ammoType, amount, src)
     ammoType = string.upper(ammoType) -- Ensure the ammo type is in uppercase to match the game's ammo types
     if type(ammoType) == "string" and type(amount) == "number" and type (src) == "number" then
         for k, v in pairs(AmmoTypes) do
-            if v == ammoType then
+            if k == ammoType then
                 local player = FeatherCore.Character.GetCharacter({ src = src })
                 local currentAmmo = MySQL.query.await("SELECT ? WHERE char_id = ?", { string.lower(ammoType), player.char.id })
-                print(currentAmmo)
-                TriggerClientEvent("feather-weapons:AddAmmo", src, ammoType, amount) break
+                if tonumber(currentAmmo[1]) >= v.max then
+                    print("FeatherWeapons.AmmoAPI.AddAmmo: Player already has max ammo for this type.") break
+                else
+                    if tonumber(currentAmmo[1]) + amount <= v.max then
+                        MySQL.query.await("UPDATE ammo SET ? = ? WHERE char_id = ?", { string.lower(ammoType), tonumber(currentAmmo[1]) + amount, player.char.id })
+                        TriggerClientEvent("feather-weapons:AddAmmo", src, ammoType, amount) break
+                    else
+                        MySQL.query.await("UPDATE ammo SET ? = ? WHERE char_id = ?", { string.lower(ammoType), v.max, player.char.id })
+                        TriggerClientEvent("feather-weapons:AddAmmo", src, ammoType, v.max - tonumber(currentAmmo[1])) break -- adding max ammo
+                    end
+                end
             end
         end
     else
-        print("AmmoAPI.AddAmmo: Invalid parameters passed.")
+        print("FeatherWeapons.AmmoAPI.AddAmmo: Invalid parameters passed.")
     end
 end
-
--- pistol max ammo 100
--- revolver max ammo 200
--- shotgun max ammo 60
--- repeater max ammo 200
--- rifle max ammo 100
--- sniper max ammo 100
--- bow max ammo 40
