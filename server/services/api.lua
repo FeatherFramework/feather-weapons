2b79feb34f00fbcf67567f19db1a5937cb837dd5
Apollyon <37031311+JusCampin@users.noreply.github.com>
2026-08-21 23:13:18 -0700

    Update api.lua

--- server/services/api.lua
+++ server/services/api.lua
@@ -1,7 +1,48 @@
+WeaponAPI = {}
+
+function WeaponAPI.GetCapabilities()
+    return {
+        contractVersion = WeaponConstants.ContractVersion,
+        ready = DefinitionRegistry.IsReady(),
+        definitions = DefinitionRegistry.Counts(),
+        inventory = InventoryAdapter.GetCapabilities(),
+        features = {
+            definitions = true,
+            metadataValidation = true,
+            characterSessions = true,
+            reconciliation = true,
+            inventoryPersistence = InventoryAdapter.IsReady(),
+            equipAuthorization = true,
+            equip = InventoryAdapter.IsReady(),
+            ammunition = InventoryAdapter.IsReady(),
+            reload = InventoryAdapter.IsReady(),
+            unload = false,
+            fireCheckpoint = InventoryAdapter.IsReady()
+        }
+    }
+end
+
+function WeaponAPI.GetDefinition(kind, id)
+    return DefinitionRegistry.Get(kind, id)
+end
+
+function WeaponAPI.ListDefinitions(kind)
+    return DefinitionRegistry.List(kind)
+end
+
+function WeaponAPI.GetRuntime(source)
+    return WeaponRuntime.Get(source)
+end
+
 exports("initiate", function()
-    local api = {}
-
-    api.ammo = AmmoAPI
-
-    return api
+    return {
+        GetCapabilities = WeaponAPI.GetCapabilities,
+        Definitions = { Get = WeaponAPI.GetDefinition, List = WeaponAPI.ListDefinitions },
+        Metadata = { Build = WeaponMetadata.Build, Validate = WeaponMetadata.Validate },
+        Runtime = { Get = WeaponAPI.GetRuntime },
+        Inventory = {
+            InstallProvider = InventoryAdapter.InstallProvider,
+            GetCapabilities = InventoryAdapter.GetCapabilities
+        }
+    }
 end)
