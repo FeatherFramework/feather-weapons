b401662c3452ee0348448474f26d35607a0f9218
Apollyon <37031311+JusCampin@users.noreply.github.com>
2026-08-21 23:13:37 -0700

    Create definitions.lua

---
+++ shared/definitions.lua
@@ -0,0 +1,40 @@
+WeaponDefinitionCatalog = {
+    ammunition = {
+        revolver_standard = {
+            id = "revolver_standard",
+            kind = "ammunition",
+            itemName = "ammo_revolver",
+            label = "Revolver Cartridges",
+            nativeAmmoName = "AMMO_REVOLVER",
+            stackable = true,
+            tags = { "revolver", "standard" }
+        }
+    },
+    weapons = {
+        revolver_cattleman = {
+            id = "revolver_cattleman",
+            kind = "weapon",
+            itemName = "cattleman_revolver",
+            label = "Cattleman Revolver",
+            nativeWeaponName = "WEAPON_REVOLVER_CATTLEMAN",
+            family = "revolver",
+            slot = "sidearm",
+            ammunitionType = "revolver_standard",
+            capacity = 6,
+            condition = {
+                minimum = 0,
+                maximum = 100,
+                equipMinimum = 1
+            },
+            attachmentSlots = {},
+            policies = {
+                transferable = true,
+                droppable = true,
+                destructible = true,
+                serialRequired = true
+            },
+            tags = { "firearm", "sidearm", "revolver" }
+        }
+    },
+    attachments = {}
+}
