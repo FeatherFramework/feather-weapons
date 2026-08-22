89cf66e783e46ba45094ecfae324616008ebd4bd
Apollyon <37031311+JusCampin@users.noreply.github.com>
2026-08-21 23:13:34 -0700

    Create constants.lua

---
+++ shared/constants.lua
@@ -0,0 +1,15 @@
+WeaponConstants = {
+    ContractVersion = 1,
+    MetadataSchemaVersion = 1,
+    DefinitionKinds = {
+        weapon = true,
+        ammunition = true,
+        attachment = true
+    },
+    WeaponSlots = {
+        melee = true,
+        sidearm = true,
+        longgun = true,
+        throwable = true
+    }
+}
