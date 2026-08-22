917b7f5b83b3e8cf8af0368d42fa7b1ecf674cd8
Apollyon <37031311+JusCampin@users.noreply.github.com>
2026-08-21 23:12:58 -0700

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,39 +1,46 @@
-fx_version "adamant"
-games {"rdr3"}
+fx_version "cerulean"
+games { "rdr3" }
 rdr3_warning "I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships."
 lua54 "yes"
 
 description "The weapon system for the Feather framework."
-author "Feather @Jake2k4"
+author "Feather Framework"
 name "feather-weapons"
-version "0.1.1"
+version "0.2.0-dev"
 
-github_version_check 'true'
-github_version_type 'release'
-github_ui_check 'false'
-github_link 'https://github.com/FeatherFramework/feather-weapons'
+github_version_check "true"
+github_version_type "release"
+github_ui_check "false"
+github_link "https://github.com/FeatherFramework/feather-weapons"
 
 shared_scripts {
-  '/shared/data/*.lua',
-  "config.lua"
+    "config.lua",
+    "/shared/constants.lua",
+    "/shared/errors.lua",
+    "/shared/definitions.lua",
+    "/shared/validation.lua"
 }
 
 server_scripts {
-  '@oxmysql/lib/MySQL.lua',
-  "/server/imports.lua",
-  "/server/services/*.lua",
-  "/server/main.lua"
+    "/server/imports.lua",
+    "/server/adapters/core.lua",
+    "/server/adapters/inventory.lua",
+    "/server/services/definition_registry.lua",
+    "/server/services/metadata.lua",
+    "/server/adapters/test_inventory.lua",
+    "/server/services/runtime.lua",
+    "/server/services/equip.lua",
+    "/server/services/ammo.lua",
+    "/server/services/reconciliation.lua",
+    "/server/services/api.lua",
+    "/server/main.lua"
 }
 
 client_scripts {
-  "/client/imports.lua",
-  "/client/services/*.lua",
-  "/client/main.lua"
+    "/client/imports.lua",
+    "/client/main.lua"
 }
 
 dependencies {
-  "oxmysql",
-  "feather-core",
-  "feather-character",
-  "feather-inventory"
+    "feather-core"
 }
