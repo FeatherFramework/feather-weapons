792e57900af2c192f7630e9624cab745921f3d13
Apollyon <37031311+JusCampin@users.noreply.github.com>
2026-08-24 03:07:10 -0700

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,50 +1,50 @@
-fx_version "cerulean"
-games { "rdr3" }
-rdr3_warning "I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships."
-lua54 "yes"
+fx_version 'cerulean'
+game 'rdr3'
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+lua54 'yes'
 
-description "The weapon system for the Feather framework."
-author "Feather Framework"
-name "feather-weapons"
-version "0.2.0"
+description 'The official weapon system for Feather framework.'
+author 'Feather Framework'
+name 'feather-weapons'
+version '0.2.1'
 
-github_version_check "true"
-github_version_type "release"
-github_ui_check "false"
-github_link "https://github.com/FeatherFramework/feather-weapons"
+github_version_check 'true'
+github_version_type 'release'
+github_ui_check 'false'
+github_link 'https://github.com/FeatherFramework/feather-weapons'
 
 shared_scripts {
-    "config.lua",
-    "/shared/constants.lua",
-    "/shared/errors.lua",
-    "/shared/definitions.lua",
-    "/shared/validation.lua"
+    'config.lua',
+    '/shared/constants.lua',
+    '/shared/errors.lua',
+    '/shared/definitions.lua',
+    '/shared/validation.lua'
 }
 
 server_scripts {
-    "/server/imports.lua",
-    "/server/adapters/core.lua",
-    "/server/adapters/inventory.lua",
-    "/server/adapters/feather_inventory.lua",
-    "/server/services/definition_registry.lua",
-    "/server/services/metadata.lua",
-    "/server/services/runtime.lua",
-    "/server/services/equip.lua",
-    "/server/services/ammo.lua",
-    "/server/services/repair.lua",
-    "/server/services/issuance.lua",
-    "/server/services/commands.lua",
-    "/server/services/reconciliation.lua",
-    "/server/services/api.lua",
-    "/server/main.lua"
+    '/server/imports.lua',
+    '/server/adapters/core.lua',
+    '/server/adapters/inventory.lua',
+    '/server/adapters/feather_inventory.lua',
+    '/server/services/definition_registry.lua',
+    '/server/services/metadata.lua',
+    '/server/services/runtime.lua',
+    '/server/services/equip.lua',
+    '/server/services/ammo.lua',
+    '/server/services/repair.lua',
+    '/server/services/issuance.lua',
+    '/server/services/commands.lua',
+    '/server/services/reconciliation.lua',
+    '/server/services/api.lua',
+    '/server/main.lua'
 }
 
 client_scripts {
-    "/client/imports.lua",
-    "/client/main.lua"
+    '/client/imports.lua',
+    '/client/main.lua'
 }
 
 dependencies {
-    "feather-core",
-    "feather-inventory"
+    'feather-core',
+    'feather-inventory'
 }
